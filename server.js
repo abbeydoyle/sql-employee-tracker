604b13a85912ceaa8321e7cf8af970bf1166974c
Abigail Doyle <abigaildoyle3@gmail.com>
2022-12-15 18:39:38 -0500

    file setup

---
+++ server.js
@@ -0,0 +1,26 @@
+// requirements
+const inquirer = require('inquirer');
+const mysql = require('mysql2');
+const cTable = require('console.table');
+
+// declare port
+const PORT = process.env.PORT || 3001;
+const app = express();
+
+// Express middleware
+app.use(express.urlencoded({ extended: false }));
+app.use(express.json());
+
+// Connect to database
+const db = mysql.createConnection(
+  {
+    host: 'localhost',
+    // MySQL username,
+    user: 'root',
+    // MySQL password
+    password: '',
+    database: 'employee_db'
+  },
+  console.log(`Connected to the employee_db database.`)
+);
+
