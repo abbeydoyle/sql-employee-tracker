2c08a25c8367f2323be0bbb2fd5a78918e1d59ac
Abigail Doyle <abigaildoyle3@gmail.com>
2022-12-15 18:54:18 -0500

    initial prompt switch case

--- server.js
+++ server.js
@@ -2,6 +2,7 @@
 const inquirer = require('inquirer');
 const mysql = require('mysql2');
 const cTable = require('console.table');
+const { listenerCount } = require('process');
 
 // declare port
 const PORT = process.env.PORT || 3001;
@@ -24,3 +25,42 @@ const db = mysql.createConnection(
   console.log(`Connected to the employee_db database.`)
 );
 
+
+
+
+// opening question
+const initialQuestion = () => {
+      inquirer.prompt ([
+            {
+                  type: `list`,
+                  name: `purpose`,
+                  message: `What would you like to do?`,
+                  options: [`View all employees`, `Add employee`, `Update employee role`, `View all roles`, `Add role`, `View all departments`, 'Add department', `Exit application`]
+            }
+      ])
+      .then(function(response) {
+            switch (response.purpose) {
+                  case "View all employees": viewEmployees();
+                  break;
+                  case "Add employee": addEmployees();
+                  break;
+                  case "Update employee role": updateEmployeeRole();
+                  break;
+                  case "View all roles": viewRoles();
+                  break;
+                  case "Add role": addRole();
+                  break;
+                  case "View all departments": viewDepartments();
+                  break;
+                  case "Add department": addDepartment();
+                  break;
+                  // TODO: write app function
+                  default: 
+                  break;
+            }
+      })
+}
+
+// TODO: view functions will be table queries
+
+// TODO: add functions will be prompts then queries
