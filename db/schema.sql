02736087804932d5ee31a510aba252c3d48f8b81
Abigail Doyle <abigaildoyle3@gmail.com>
2022-12-15 20:12:04 -0500

    create seedssql based off demo video

--- db/schema.sql
+++ db/schema.sql
@@ -6,24 +6,25 @@ USE employee_db
 
 CREATE TABLE employees (
       employee_id INT AUTO_INCREMENT PRIMARY KEY,
-      first_name VARCHAR(30),
-      last_name VARCHAR(30),
-      -- TODO: populate from existing data
-      employee_role
-      employee_manager 
+      first_name VARCHAR(30) NOT NULL,
+      last_name VARCHAR(30) NOT NULL,
+      role_id INT NOT NULL
+      manager_id INT NOT NULL 
 )
 
 
 
 CREATE TABLE roles (
-      role_name VARCHAR(30)
-      -- TODO: populate from existing data
-      role_department VARCHAR(30)
+      role_id INT AUTO_INCREMENT PRIMARY KEY,
+      role_name VARCHAR(30) NOT NULL,
+      -- FIXME: currency symbol
       role_salary INT
+      department_id INT NOT NULL
 )
 
 
 
 CREATE TABLE departments (
-      deparment_name VARCHAR(30)
+      department_id INT AUTO_INCREMENT PRIMARY KEY,
+      deparment_name VARCHAR(30) NOT NULL
 )
--- db/seeds.sql
+++ db/seeds.sql
@@ -1,2 +1,39 @@
 -- data populating the database at the time it is created
 
+INSERT INTO employees 
+      (first_name, last_name, role_id, manager_id)
+
+VALUES
+      (`Ashley`, `Rodriguez`, `Lead Engineer`, 1, NULL),
+      (`Kevin`, `Tupik`, `Software Engineer`, 1, 1),
+      (`Kunal`, `Singh`, `Account Manager`, 2, NULL),
+      (`Malia`, `Brown`, `Accountant`, 2, 3),
+      (`Sarah`, `Lourd`, `Legal Team Lead`, 3, NULL),
+      (`Tom`, `Allen`, `Lawyer`, 3, 5),
+      (`John`, `Doe`, `Sales Lead`, 4, NULL),
+      (`Mike`, `Chan`, `Salesperson`, 4, 7),
+
+
+
+INSERT INTO roles 
+      (role_name, role_salary, department_id)
+
+VALUES
+      (`Lead Engineer`, `150000`, 1),
+      (`Software Engineer`, `120000`, 1),
+      (`Account Manager`, `160000`, 2),
+      (`Accountant`, `125000`, 2),
+      (`Legal Team Lead`, `250000`, 3),
+      (`Lawyer`, `190000`, 3),
+      (`Sales Lead`, `100000`, 4),
+      (`Salesperson`, `80000`, 4),
+
+
+INSERT INTO departments
+      (deparment_name)
+
+VALUES
+      (`Engineering`),
+      (`Finance`),
+      (`Legal`),
+      (`Sales`),
