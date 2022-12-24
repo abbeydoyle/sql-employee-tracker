9b1095c33b7d0091933cb8ca31cd1c057124bbba
Abigail Doyle <abigaildoyle3@gmail.com>
2022-12-23 21:22:54 -0500

    fix seeds and schema syntax errors

--- db/schema.sql
+++ db/schema.sql
@@ -15,23 +15,22 @@ CREATE TABLE department (
 );
 
 CREATE TABLE role (
-      id INT AUTO_INCREMENT NOT NULL PRIMARY KEY,
+      id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
       title VARCHAR(30) NOT NULL,
       salary DECIMAL NOT NULL,
       department_id INT,
-      FOREIGN KEY (department_id),
-      REFERENCES department(id),
+      FOREIGN KEY (department_id)
+      REFERENCES department(id)
       ON DELETE SET NULL
 );
 
 CREATE TABLE employee (
-      id INT AUTO_INCREMENT NOT NULL PRIMARY KEY,
+      id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
       first_name VARCHAR(30) NOT NULL,
-      last_name varchar(30) NOT NULL,
-      FOREIGN KEY (role_id),
-      REFERENCES role(id),
-      ON DELETE SET NULL,
-      FOREIGN KEY (manager_id),
-      FOREIGN KEY employee(id),
+      last_name VARCHAR(30) NOT NULL,
+      role_id INT,
+      manager_id INT,
+      FOREIGN KEY (role_id)
+      REFERENCES role(id)
       ON DELETE SET NULL
 );
--- server.js
+++ server.js
@@ -3,7 +3,14 @@ const express = require(`express`);
 const inquirer = require('inquirer');
 const mysql = require('mysql2');
 const cTable = require('console.table');
+const fs = require("fs");
 require("dotenv").config();
+
+// strings containing sql queries to be called on
+// suggested to keep separate files for view function queries
+const queryDepartments = fs.readFileSync("db/queryDepartments.sql").toString();
+const queryRoles = fs.readFileSync("db/queryRoles.sql").toString();
+const queryEmployees = fs.readFileSync("db/queryEmployees.sql").toString();
 
 // declare port
 const PORT = process.env.PORT || 3001;
@@ -31,6 +38,11 @@ const db = mysql.createConnection(
       console.log(`Database connection successful via id` + db.threadID);
       initialQuestion();
 });
+
+// empty arrays for responsive inquirer list choices
+let roles = [];
+
+
 
 // opening question
 const initialQuestion = () => {
@@ -63,6 +75,109 @@ const initialQuestion = () => {
                   console.log(`Thank you for visiting!`);
                   break;
             }
+      });
+
+      // used for responsive role list in add function inquirer prompt
+      db.query(`SELECT role.title AS title FROM role;`, (err, result) => {
+            if (err) throw err;
+            let roles = result.map(function(obj) {
+                  return obj.title;
+                });
+      })
+
+      // used for responsive manager list in add function inquirer prompt
+      db.query(`SELECT CONCAT(manager.first_name, " ", manager.last_name) AS manager FROM employee LEFT JOIN employee manager ON employee.manager_id = manager.id WHERE manager.id > 0 GROUP BY manager.first_name, manager.last_name;`, (err, result) => {
+            if (err) throw err;
+            let managers = result.map(function(obj) {
+                  return obj.manager
+            })
       })
 };
 
+viewEmployees = () => {
+      db.query(`${queryEmployees}`, (err, result) => {
+            if (err) throw err;
+            console.table(result);
+            initialQuestion();
+      })
+};
+
+viewRoles = () => {
+      db.query(`${queryRoles}`, (err, result) => {
+            if (err) throw err;
+            console.table(result);
+            initialQuestion();
+      })
+};
+
+viewDepartments = () => {
+      db.query(`${queryDepartments}`, (err, result) => {
+            if (err) throw err;
+            console.table(result);
+            initialQuestion();
+      })
+};
+
+addEmployees = () => {
+      inquirer.prompt ([
+            {
+                  type: `input`,
+                  name: `employeeFirst`,
+                  message: `What's the first name of this employee?`,
+                  validate: employeeFirstInput => {
+                        if (employeeFirstInput) {
+                              return true;
+                        } else {
+                              console.log(`Please enter your employee's  first name`);
+                              return false;
+                        }
+                  }
+            },
+
+            {
+                  type: `input`,
+                  name: `employeeLast`,
+                  message: `What's the last name of this employee?`,
+                  validate: employeeLastInput => {
+                        if (employeeLastInput) {
+                              return true;
+                        } else {
+                              console.log(`Please enter your employee's  last name`);
+                              return false;
+                        }
+                  }
+            },
+
+            {
+                  type: `input`,
+                  name: `employeeRole`,
+                  message: `What's this employee's role?`,
+                  validate: employeeRoleInput => {
+                        if (employeeRoleInput) {
+                              return true;
+                        } else {
+                              console.log(`Please enter your employee's  role`);
+                              return false;
+                        }
+                  }
+            },
+
+            {
+                  type: `input`,
+                  name: `employeeManager`,
+                  message: `Who is the manager of this employee?`,
+                  validate: employeeManagerInput => {
+                        if (employeeManagerInput) {
+                              return true;
+                        } else {
+                              console.log(`Please enter your employee's  manager`);
+                              return false;
+                        }
+                  }
+            }
+      ])
+      .then((answers) => {
+            db.query(``)
+
+      })
+}
